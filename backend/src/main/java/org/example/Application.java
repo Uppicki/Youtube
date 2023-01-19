13677969fc930973f81b3e671c040170b08190dd
Uppicki <81292510+Uppicki@users.noreply.github.com>
2023-01-20 02:16:04 +0300

    Init

---
+++ backend/src/main/java/org/example/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    /**
+     * org.example.Application's main method
+     *
+     * @param args the console arguments
+     */
+    public static void main(String[] args) {
+        // Run the app!
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    /**
+     * org.example.Application's main method
+     *
+     * @param args the console arguments
+     */
+    public static void main(String[] args) {
+        // Run the app!
+        SpringApplication.run(Application.class, args);
+    }
+}
